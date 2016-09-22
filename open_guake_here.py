63030aa52093d1bf0fedaf721ea571aab42a7cb6
ksamuel <ksamuel@users.noreply.github.com>
2016-09-22 16:54:24 +0200

    Better use of guake options
    - Handle spaces and non ascii characters in names. 
    - Use --show instead of toggle.
    - Rename the current tab to the dir name.

--- open_guake_here.py
+++ open_guake_here.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-    This module adds a menu item to the nautilus right-click menu which allows to
-   open the guake terminal with a new tab set on the current directory
-"""
+"""This module adds a menu item to the nautilus right-click menu which allows to Open the Terminal
+   on the Selected Folder/Current Directory at predefined Geometry just through the right-clicking"""
 
+#   open-terminal-geometry.py version 3.0
+#
+#   Copyright 2009-2011 Giuseppe Penone <giuspen@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#   MA 02110-1301, USA.
 
 from gi.repository import Nautilus, GObject, Gtk, GdkPixbuf
 import urllib, os, subprocess
@@ -13,8 +29,8 @@ import locale, gettext
 
 APP_NAME = "nautilus-pyextensions"
 LOCALE_PATH = "/usr/share/locale/"
+GEOMETRY = "100x25"
 ICONPATH = "/usr/share/icons/gnome/48x48/apps/terminal.png"
-
 # internationalization
 locale.setlocale(locale.LC_ALL, '')
 gettext.bindtextdomain(APP_NAME, LOCALE_PATH)
@@ -37,31 +53,36 @@ class OpenTerminalGeometry(GObject.GObject, Nautilus.MenuProvider):
         except: pass
 
     def run(self, menu, selected):
-        """Runs the Open Guake here on the given Directory"""
+        """Runs the Open guake here on the given Directory"""
         uri_raw = selected.get_uri()
         if len(uri_raw) < 7: return
-        curr_dir = urllib.unquote(uri_raw[7:])
-        if os.path.isfile(curr_dir): curr_dir = os.path.dirname(curr_dir)
-        bash_string = "guake -t -n '" + curr_dir + "'"
-        subprocess.call(bash_string, shell=True)
+        cur_dir = urllib.unquote(uri_raw[7:]).decode('utf8')
+        if os.path.isfile(cur_dir): cur_dir = os.path.dirname(cur_dir)
+
+        subprocess.call([
+            u'guake',
+            u'--new-tab=%s' % cur_dir,
+            u'--show',
+            u'--rename-current-tab=%s' % os.path.basename(cur_dir)
+        ])
 
     def get_file_items(self, window, sel_items):
-        """Adds the 'Open Guake here' menu item to the Nautilus right-click menu,
+        """Adds the 'Open guake here' menu item to the Nautilus right-click menu,
            connects its 'activate' signal to the 'run' method passing the selected Directory/File"""
         if len(sel_items) != 1 or sel_items[0].get_uri_scheme() != 'file': return
-        item = Nautilus.MenuItem(name='NautilusPython::guake',
+        item = Nautilus.MenuItem(name='NautilusPython::terminal',
                                  label=_('Open Guake Here'),
-                                 tip=_('Open the Guake terminal on the Current/Selected Directory'),
+                                 tip=_('Open Guake on the Current/Selected Directory'),
                                  icon='terminal')
         item.connect('activate', self.run, sel_items[0])
         return [item]
 
     def get_background_items(self, window, current_directory):
-        """Adds the 'Open Guake here' menu item to the Nautilus right-click menu,
+        """Adds the 'Open guake here' menu item to the Nautilus right-click menu,
            connects its 'activate' signal to the 'run' method passing the current Directory"""
-        item = Nautilus.MenuItem(name='NautilusPython::guake',
+        item = Nautilus.MenuItem(name='NautilusPython::terminal',
                                  label=_('Open Guake Here'),
-                                 tip=_('Open the Guake terminal on the Current Directory'),
+                                 tip=_('Open Guake on the Current Directory'),
                                  icon='terminal')
         item.connect('activate', self.run, current_directory)
         return [item]
